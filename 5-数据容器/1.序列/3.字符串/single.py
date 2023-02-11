4b988cfce83e415e2569f7f722e1cdf383a6d518
wangyouzheng <353735348@qq.com>
2023-02-11 19:31:47 +0800

    2023/02/11  17:31 str

---
+++ 5-数据容器/1.序列/3.字符串/single.py
@@ -0,0 +1,18 @@
+name = 'abacstrasdb cab '
+' a b a cstr a sdb c a b '
+
+# 下标取值 0开始
+# print(name[2])
+# # index获取指定元素的第一个的下标
+# print(name.index('a'))
+#
+# print(name.index('as'))
+
+# new_name = name.replace('as', 'bbbbbbb') # as替换为bbbbbbb
+# print(new_name)
+# name = '小狗,小猫,小老虎,辣条,鬼子姜'
+# new_name = name.split(",")
+# print(new_name)
+# print(type(new_name))
+
+name.strip()
