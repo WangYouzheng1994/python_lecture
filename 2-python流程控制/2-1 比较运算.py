022954279318e7f6bd66f7baab958609ffd975a0
wangyouzheng <353735348@qq.com>
2023-02-13 14:59:34 +0800

    比较运算符 None值的处理

--- 2-python流程控制/2-1 比较运算.py
+++ 2-python流程控制/2-1 比较运算.py
@@ -11,3 +11,7 @@ my_boolean = (num_a >= 1)
 print(my_boolean)
 print(type(my_boolean)) # <class 'bool'>
 # 数据类型为布尔boolean  两种值 True  False。
+# boolean类型的匹配方式
+print(False == False)
+# 空值的匹配方式
+print(None is not None)
