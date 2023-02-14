9b4999678aa0cfd93317ade28465a3e9e0585cec
wangyouzheng <353735348@qq.com>
2023-02-14 14:26:31 +0800

    面向对象

---
+++ 11-python的面向对象/1-类与对象的认识.py
@@ -0,0 +1,24 @@
+"""
+class 类名
+类：认为是现实生活中每种物品或者客观存在的物体的一种抽象的描述。它具备属性与行为。就像人有身高属性，吃饭睡觉的行为一样。
+
+类名的表写格式要求：
+1. 首字母大写
+"""
+# 设计类
+class Student:
+    name = None
+    gender = None
+
+# 创建对象
+stu_1 = Student()
+stu_2 = Student()
+
+# 为创建的对象的属性进行赋值
+stu_1.name = '肖飞'
+stu_1.gender = '男'
+
+stu_2.name = '小姜'
+stu_2.gender = '女'
+
+
---
+++ 11-python的面向对象/2-成员方法.py
@@ -0,0 +1,54 @@
+class Student:
+    # 属性 称之为成员变量
+    name = None
+    gender = None
+
+    # 行为 成员方法
+    def say_my_name(self):
+        """
+        输出自己的name
+        :return:
+        """
+        print(self.name)
+
+    def eat(self):
+        """
+        输出自己的吃
+        :return:
+        """
+        print('到点了到点了 得吃饭了！')
+
+    def run(a, self, b):
+        """
+        test self
+        :param self:
+        :return:
+        """
+        print(a)
+
+        print(b)
+        print(self)
+        print(a.name)
+
+
+# 创建对象
+stu_1 = Student()
+
+# 为创建的对象的属性进行赋值
+stu_1.name = '肖飞'
+#
+# stu_1.say_my_name()
+# stu_1.eat()
+stu_1.run("a", b=123)
+
+"""
+成员方法和普通函数的定义 有区别，核心就是参数列表中的必须要有self且self得定义到第一个位置
+self的作用是，如果要访问自身对象中的成员变量/成员方法的时候就需要用此self
+
+经过run()的传入参数测试，其实是成员方法的第一个参数就是self，我们可以给他重命名，而且传参的时候 他是透明的。
+
+总结：
+1.self相当于其他编程语言中的this
+2.在class的funciton中默认第一个参数就是self
+3.在调用成员方法的时候self认为是透明的不存在的。
+"""
