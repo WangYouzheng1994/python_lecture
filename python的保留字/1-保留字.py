d1e1ce033c369195f8d5c7eab003fc70e427ada2
wangyouzheng <353735348@qq.com>
2023-02-15 14:15:14 +0800

    python的保留字

---
+++ python的保留字/1-保留字.py
@@ -0,0 +1,16 @@
+# 1. True、False
+# 2. and、or、not
+# 3.class
+# 4.is、in
+# 5.try、except、finally、raise
+# 6.if、elif、else
+# 7.def、global、nonlocal、return、yield
+# 8.for、while
+# 9.break、continue
+# 10.assert
+# 11.del
+# 12.import、from、 as
+# 13.with
+# 14.pass
+# 15.await、async
+# 16.lambda
