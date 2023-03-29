49b2462c68df35629e766418b4fc69b4921d3a60
wangyouzheng <353735348@qq.com>
2023-03-29 17:44:14 +0800

    数组的类型修改和去重

---
+++ 17-机器学习/4-numpy/08_数组的类型修改和去重.py
@@ -0,0 +1,17 @@
+import numpy as np
+
+float_uniform = np.random.uniform(0, 500, (2, 8))
+print(f'source_arr:{float_uniform}, dtype:{float_uniform.dtype}')
+# 元素全部转换成 int32
+target_arr = float_uniform.astype(np.int32)
+print(f'astype_target:{target_arr}, dtype:{target_arr.dtype}')
+
+# 转成byte
+print(target_arr.tostring())
+
+# 数组的去重
+multi_item_arr = np.array([1,2,2,3])
+print(multi_item_arr.size)
+unique_arr = np.unique(multi_item_arr)
+print(unique_arr)
+print(type(unique_arr))
